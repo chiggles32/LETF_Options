578e8d97812529b603ef9bde22d650489882d823
chiggles32 <charliejl32@outlook.com>
2024-02-25 22:41:12 -0500

    analysis on puts and calls

--- LETF PC backtest.R
+++ LETF PC backtest.R
@@ -18,7 +18,7 @@ setwd("C:/Users/Charlie/Desktop/R Stuff/Cleaned_Option_Data")
 #Calendars
 load_quantlib_calendars("UnitedStates/NYSE", from = "2023-10-29", to = Sys.Date())
 
-trade_days = bizseq("2023-10-29", Sys.Date() - 60 , cal = "QuantLib/UnitedStates/NYSE")
+trade_days = bizseq("2023-10-29", Sys.Date() - 70 , cal = "QuantLib/UnitedStates/NYSE")
 
 gathered = file.exists(paste0("Options_Pull_", trade_days))
 
@@ -240,147 +240,154 @@ test |>
   summarise(Count = n()) |>
   view()
 
+test2 = test1 |>
+  filter(!is.na(ImpVol))
 
-gtest = test |>
+gtest = test2 |>
   filter((Bid != 0 & Ask != 0)) |>
   group_by(U, QuoteTime, Expiry, Strike) |>
   # arrange(Strike, .by_group = T) |>
   # mutate(gvar = cur_group_id()) |>
   distinct() |>
-  filter(n() == 2, Expiry < Sys.Date()) 
+  filter(Expiry < Sys.Date()) 
 
 ##how to ensure price is accurate
 
 ctest = gtest |>
-  summarize(
-    C1 = first(Contract.Name),
-    C2 = last(Contract.Name),
-    Quoted = as.POSIXct(first(QuoteTime)),
-    IV1 = first(Scaled.ImpVol),
-    IV2 = last(Scaled.ImpVol),
-    LM1 = first(Scaled.LM),
-    LM2 = last(Scaled.LM),
-    Expiry = first(Expiry),
-    ET = first(ET),
-    Start_Price = sum(Price),
-    End_Price = sum(Expiry_Price),
-    Strike = first(Strike),
-    S01 = first(S0),
-    S0t = first(Ending_Price)
-  ) |>
-  mutate(End_Price = if_else(is.na(End_Price), abs(S0t - Strike), End_Price),
-         ET = if_else(is.na(ET), as.numeric(as.POSIXct(paste0(Expiry, " 16:00:00"))), ET),
-         deltat = time_length(difftime(QuoteTime, ET), unit = "day"),
-         logret = log(End_Price/Start_Price),
-         RP = if_else(U == "QQQ", (Start_Price - abs(Strike-S01))/ Strike, ((Start_Price - abs(Strike-S01))/ Strike) * (1/3)))
+  filter(!is.na(Ending_Price)) |>
+  select(Contract.Name, Strike, QuoteTime, HVol, U, Expiry, Price, S0, theta, MeanVol, Scaled.LM, Scaled.ImpVol, Scaled.RP, Expiry_Price, ET, Ending_Price, Analog) |>
+  mutate(ET = if_else(is.na(ET), as.numeric(as.POSIXct(paste0(Expiry, " 16:00:00"))), ET),
+         deltat = time_length(difftime(QuoteTime, ET), unit = "day"))
 
+  
+  
 
 ###Clean this up then move on to create a way to compare across and profits.
 ## basic buy strangle comparison + correlation to market.
 
-sample_df = ctest |>
+# creating list of exp/qt
+p.c = ctest |>
   ungroup() |>
-  filter(Expiry == "2023-11-10", QuoteTime == 1698674472) |>
-  rowwise() |>
-  mutate(Moneyness = if_else(Strike > S01, abs(LM1), -abs(LM1)),
-         ImpVol = if_else( (is.na(IV1) | is.na(IV2) ), sum(IV1,IV2, na.rm = TRUE) , sum(IV1, IV2) / 2 )
-         # Moneyness = if_else(U == "SQQQ", -Moneyness, Moneyness)
-         ) |>
-  ungroup() |>
-  mutate(ID = row_number()) |>
-  select(-IV1, -IV2, -LM1, -LM2) 
+  group_by(Analog) |>
+  group_split()
 
-
-
-X = sample_df |>
-  select(U, Moneyness, ID)
-# moneyness matching function
-
-# listed out expiry qt
-
-Y = X |>
-  expand_grid(Comparison = X) |> 
-  filter(U != Comparison$U) |>
-  mutate(Difference = abs(Moneyness - Comparison$Moneyness)) |>
-  group_by(ID, Comparison_U = Comparison$U) |>
-  slice_min(Difference, n = 1) |>
-  unnest_wider(col = Comparison, names_repair = "unique") |>
-  select(ID = ID...3, Comparison_U, Comparison_ID = ID...6, Difference) |>
-  pivot_wider(names_from = Comparison_U, values_from = Comparison_ID) |>
-  ungroup() 
-
-Z = Y |>
-  select(-Difference)
-
-row.f = function(r){
-  as.vector(na.omit(as.numeric(r)))
+group.exp.qt = function(x){
+  x |>
+    ungroup() |>
+    group_by(Expiry, QuoteTime) |>
+    filter(n_distinct(U) != 1) |>
+    mutate(ID = row_number()) |>
+    group_split()
 }
 
-Z = t(apply(Z,1,row.f))
+p.c.x = lapply(p.c, group.exp.qt)
 
-Z.s = t(apply(Z,1,sort)) |>
-  as.data.frame() |>
-  duplicated()
+Calls = p.c.x[[1]]
+Puts = p.c.x[[2]]
 
-Y = Y |>
-  filter(!Z.s) 
-Y = Y |>
-  pivot_longer(cols = SQQQ:QQQ, values_drop_na = TRUE) 
+  
 
-Q = map2_df(Y$ID, Y$value, ~ {
-  row1 = sample_df[.x, , drop = FALSE]
-  row2 = sample_df[.y, , drop = FALSE]
+
+
+match.fun = function(J) {
+  X = J |>
+    select(U, Scaled.LM, ID)
+  Y = X |>
+    expand_grid(Comparison = X) |> 
+    filter(U != Comparison$U) |>
+    mutate(Difference = abs(Scaled.LM - Comparison$Scaled.LM)) |>
+    group_by(ID, Comparison_U = Comparison$U) |>
+    slice_min(Difference, n = 1) |>
+    unnest_wider(col = Comparison, names_repair = "unique") |>
+    select(ID = ID...3, Comparison_U, Comparison_ID = ID...6, Difference) |>
+    pivot_wider(names_from = Comparison_U, values_from = Comparison_ID) |>
+    ungroup() 
   
-  crow = cbind(row1, row2)
+  Z = Y |>
+    select(-Difference)
   
-  colnames(crow) = make.unique(c(colnames(row1), colnames(row2)))
+  row.f = function(r){
+    as.vector(na.omit(as.numeric(r)))
+  }
   
-  return(crow)
-})
+  Z = t(apply(Z,1,row.f))
+  
+  Z.s = t(apply(Z,1,sort)) |>
+    as.data.frame() |>
+    duplicated()
+  
+  
+  Y = Y |>
+    filter(!Z.s) 
+  Y = Y |>
+    pivot_longer(cols = 3:dim(Y)[2], values_drop_na = TRUE) 
+  
+  # Need to either do this calculation as a grouped df or map with multiple data frames
+  
+  Q = map2_df(Y$ID, Y$value, ~ {
+    row1 = J[.x, , drop = FALSE]
+    row2 = J[.y, , drop = FALSE]
+    
+    crow = cbind(row1, row2)
+    
+    colnames(crow) = make.unique(c(colnames(row1), colnames(row2)))
+    
+    return(crow)
+  })
+  
+}  
+
+Matched_Calls = lapply(Calls, match.fun)
+Matched_Puts = lapply(Puts, match.fun)
+
+final = do.call(rbind, Matched_Calls) |>
+  mutate(TTE = abs(deltat),
+         M_Dif = abs(Scaled.LM - Scaled.LM.1),
+         Rn = row_number(),
+         RP_Dif = abs(Scaled.RP - Scaled.RP.1))
+
+
+final = final |>
+  mutate(Simple_Ret = (Expiry_Price - Price)/Price,
+         Simple_Ret.1 = (Expiry_Price.1 - Price.1)/Price.1,
+         SRet_Dif = abs(Simple_Ret - Simple_Ret.1),
+         LongShort = if_else(
+           Scaled.ImpVol > Scaled.ImpVol.1,
+           paste(sub("\\d.*", "", Contract.Name), sub("\\d.*", "", Contract.Name.1), sep = ">"),
+           paste(sub("\\d.*", "", Contract.Name.1), sub("\\d.*", "", Contract.Name), sep = ">")
+         )
+  )
 
 # functionize? the above, then create the final dataframe for regression
 # fix ret calculation
 # why is it showing the opposite of what it should?
 
 
-rdf = Q |>
-  mutate(IV_Dif = abs(ImpVol - ImpVol.1),
-         TTE = abs(deltat),
-         M_Dif = abs(Moneyness - Moneyness.1),
-         Rn = row_number(),
-         RP_Dif = abs(RP - RP.1),
-         Simple_Ret = (End_Price - Start_Price)/Start_Price,
-         Simple_Ret.1 = (End_Price.1 - Start_Price.1)/Start_Price.1,
-         SRet_Dif = abs(Simple_Ret - Simple_Ret.1),
-         LongShort = if_else(
-           ImpVol < ImpVol.1,
-           paste(sub("\\d.*", "", C1), sub("\\d.*", "", C1.1), sep = ">"),
-           paste(sub("\\d.*", "", C1.1), sub("\\d.*", "", C1), sep = ">")
-         )) 
-  
-
-
- tr = rdf #|>
-   #filter(M_Dif < .002, abs(Moneyness) < .025)
+tr = final |>
+  filter(M_Dif < .001)
 
 tr = tr |>
-  filter(grepl("SQQQ", tr$LongShort)) |>
-  mutate(LS_Ret = if_else(ImpVol > ImpVol.1,
+  mutate(LS_Ret = if_else(Scaled.ImpVol > Scaled.ImpVol.1,
                           (-Simple_Ret + Simple_Ret.1)/2,
-                          (Simple_Ret - Simple_Ret.1)/2))
+                          (Simple_Ret - Simple_Ret.1)/2),
+         IV_Dif = abs(Scaled.ImpVol-Scaled.ImpVol.1)) |>
+  filter(IV_Dif > .02)
 
-model = lm(LS_Ret ~ IV_Dif + M_Dif + RP_Dif  + IV_Dif * RP_Dif  , data = tr)
+model = lm(LS_Ret ~ IV_Dif , data = tr)
 summary(model)
 par(mfrow=c(2,2)) # Set up the plotting area to display 4 plots at once
 plot(model) 
 
-# how to accurately map the moneyness difference for sqqq because of opposite direction
+###Why oh why 
 
 tr |>
-  filter(LS_Ret != 0) |>
+  filter(LS_Ret != 0,
+         LongShort == "SQQQ>TQQQ" | LongShort == "TQQQ>SQQQ") |>
   ggplot( aes(x = IV_Dif, y = LS_Ret, color = LongShort)) +
   geom_point()
 
+t.stats = tr |>
+  select(LS_Ret, IV_Dif)
+
 new_data = data.frame(IV_Dif = seq(from = .01, to = .5, by = .05))
 predict(model, new_data)
-
